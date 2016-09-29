509d0f5d924be9d2dace22d3d260ead980a4b7dd
Robert Polevoi <robert.polevoi@gmail.com>
2016-09-28 18:00:17 -0700

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 
 import { AppComponent }   from './app.component';
-import { routing, appRoutingProviders }  from './app.routing';
+import { routing }  from './app.routing';
 
 import { RouteAComponent } from './route_a/route_a.component';
 import { RouteBComponent } from './route_b/route_b.component';
@@ -12,7 +12,7 @@ import { StoreService } from './store.service';
 @NgModule({
   imports: [BrowserModule, routing],
   declarations: [ AppComponent, RouteAComponent, RouteBComponent ],
-  providers: [appRoutingProviders,StoreService ],
+  providers: [ StoreService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
